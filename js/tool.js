c38edd500a992a6d74b7f8e8dbe24a6ceb9d9c11
pandaLv <pandalv@foxmail.com>
2016-04-21 18:48:01 +0800

    first commit

---
+++ js/tool.js
@@ -0,0 +1,158 @@
+/**
+ * Created by jmupanda on 2016/3/10.
+ */
+window.toolByPandaLv = {
+    //ajax
+    myAjax: function(options) {
+        var request = new XMLHttpRequest();
+        if (!options.url) {
+            return false;
+        }
+        if (options.method.toLocaleLowerCase() === 'get'){
+            var url = '';
+            url += options.url;
+            for (var a in options.data) {
+                if (options.data.hasOwnProperty(a)) {
+                    url += (url.indexOf("?") == -1 ? "?" : "&");
+                    url += encodeURIComponent(a) + "=" + encodeURIComponent(options.data[a]);
+                }
+            }
+
+
+            request.open(options.method, url, true);
+            request.send(null);
+        } else if (options.method.toLocaleLowerCase() === 'post') {
+            request.open(options.method, options.url, true);
+            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            request.send(options.data);
+        } else {
+            return;
+        }
+
+        request.onload = function() {
+            options.onload && options.onload();
+        };
+
+        request.onreadystatechange = function(){
+            if (request.readyState == 4) {
+                if (request.status >= 200 && request.status < 400) {
+                    // Success!
+                    options.success(JSON.parse(request.responseText));
+                } else {
+                    options.error();
+                    // We reached our target server, but it returned an error
+                }
+            }
+        };
+
+        request.onerror = function() {
+            options.error && options.error();
+            // There was a connection error of some sort
+        };
+        request.timeout = options.timeoutTime || 15000; //将超时设置为15 秒钟（仅适用于IE8+）
+        request.ontimeout = function(){
+            console.error('TimeOut');
+            options.timeout && options.timeout()
+        };
+    },
+    //
+    stopRolling: function(e, touch, dom) {
+        // 高位表示向上滚动
+        // 底位表示向下滚动
+        // 1容许 0禁止
+        var status = '11';
+        var currentY = e.touches[0].clientY;
+        if (dom.scrollTop === 0) {
+            // 如果内容小于容器则同时禁止上下滚动
+            status = dom.offsetHeight >= dom.scrollHeight ? '00' : '01';
+        } else if (dom.scrollTop + dom.offsetHeight >= dom.scrollHeight) {
+            // 已经滚到底部了只能向上滚动
+            status = '10';
+        }
+        if (status != '11') {
+            // 判断当前的滚动方向
+            var direction = currentY - touch.y > 0 ? '10' : '01';//10为向上滚动,01为向下滚动
+            // 操作方向和当前允许状态求与运算，运算结果为0，就说明不允许该方向滚动，则禁止默认事件，阻止滚动
+            if (!(parseInt(status, 2) & parseInt(direction, 2))) {
+                e.preventDefault();
+            }
+        }
+    },
+    //
+    //加载等待页方法
+    loadingPage : (function(){
+            var loadingPageEle = {},
+                loadingPageStyle = {
+                    text: '加载中',
+                };
+            return {
+                loadingPageInit: function(dom){
+                    loadingPageEle = document.getElementById('page-loading');
+                    if (!loadingPageEle) {
+                        dom = document.querySelector(dom || 'body');
+                        //主加载页面
+                        var loadingPage = document.createElement('div');
+                        loadingPage.id = 'page-loading';
+                        loadingPage.setAttribute('style', 'display:none; ' +
+                            'position:absolute; top:0;' +
+                            'background-color:rgba(0, 0, 0, 0.6);'+
+                            'z-index:30; width: 100%; height: 100%;');
+
+                        //普通加载信息
+                        var info = document.createElement('div');
+                        info.setAttribute('id', 'page-loading-info');
+                        info.setAttribute('style', 'position: absolute;' +
+                            'left: 50%;top: 50%;' +
+                            'margin-top:-50px;margin-left: -15%;' +
+                            'width: 25%;padding:10px;border-radius: 5px;' +
+                            'text-align: center; ' +
+                            'background-color: rgba(0, 0, 0, 0.6);');
+                        info.innerHTML = '<img style="height:40px" src="image/loading.gif" />' +
+                            '<p style=" margin-top:10px;font-size:14px;color:#fff;">加载中..</p>' +
+                            '<div id = "page-loading-progress" hidden="hidden" style="margin-top: 10px;  width: 100%;height:5px;border-radius: 2px;background-color: #fff">' +
+                            '<div style="width:0;height:100%;background-color: #19aaf2; border-radius: 2px;"></div></div>';
+                        loadingPage.appendChild(info);
+
+                        dom.appendChild(loadingPage);
+                        loadingPageEle = document.getElementById('page-loading');
+                    }
+                },
+                loadingPageOpen: function(data) {
+                    if (data) {
+                        if (data.text) {
+                            loadingPageEle.querySelector('#page-loading-info').children[1].innerHTML = data.text;
+                            loadingPageStyle = data.text;
+                        }
+                        if (data.mode) {
+                            switch (data.mode) {
+                                case 'progress' :
+                                    loadingPageEle.querySelector('#page-loading-progress').removeAttribute('hidden');
+                                    break;
+                                case 'normal' :
+                                    loadingPageEle.querySelector('#page-loading-progress').setAttribute('hidden', 'hidden');
+                                    break;
+                                default :
+
+                            }
+
+                        }
+                    }
+                    loadingPageEle.style.display = 'block';
+                },
+                processChange: function(now, max) {
+                    if (!now || !max) {
+                        console.error('没有传入正确的值');
+                        return
+                    }
+                    var infoEle = loadingPageEle.querySelector('#page-loading-info'),
+                        textEle = infoEle.children[1],
+                        progressEle = infoEle.children[2];
+                    textEle.innerHTML = loadingPageStyle.text + '(' + now + '/' + max + ')';
+                    progressEle.children[0].style.width = now / max;
+                },
+                loadingPageClose: function() {
+                   loadingPageEle.style.display = 'none';
+                }
+            }
+        })(),
+};
---
+++ js/upload.js
@@ -0,0 +1,308 @@
+(function(){
+    window.onload = function(){
+        var board = document.getElementById("board_upload");
+        board.style.height = window.innerHeight - 145 + 'px';
+        var sizeNum = (window.innerWidth - 60) / 3,
+            size = sizeNum + 'px',
+            hiddenImg = document.getElementById('hiddenImgEle');
+        var filesData = [], deleteTrack = [];
+        var deleteMode = false;
+        var upload = document.getElementById('uploadDiv');
+        upload.style.width = size;
+        upload.style.height = size;
+
+        //初始化等待页面
+        var clickTime = 0;
+        toolByPandaLv.loadingPage.loadingPageInit();
+
+        upload.addEventListener('change', function(e){
+
+            try {
+                var files = this.children[0].files;
+            } catch (error) {
+                alert("您当前的浏览器不支持上传.");
+                return;
+            }
+
+            if (files.length) {
+                var mime = {'png': 'image/png', 'jpg': 'image/jpeg', 'jpeg': 'image/jpeg', 'bmp': 'image/bmp'};
+                for (var j = 0, m = files.length; j < m; j += 1) {
+                    var file = files[j];
+                    // 如果没有文件类型，则通过后缀名判断（解决微信及360浏览器无法获取图片类型问题）
+                    if (!file.type) {
+                        file.type = mime[file.name.match(/\.([^\.]+)$/i)[1]];
+                    }
+                    if (!/image.(png|jpg|jpeg|bmp)/.test(file.type)) {
+                        alert('选择的文件类型不是图片');
+                        return;
+                    }
+                    file.url = createObjectURL(file);
+                    file.no = filesData.length ? filesData[filesData.length - 1].no + 1 : 1;
+                    filesData.push(file);
+                    appendImgEle(file);
+                }
+            }
+        }, false);
+
+        //click事件检测多次点击添加图片没有反映
+        upload.onclick = function() {
+            if (typeof (clickTime) === 'number') {
+                if (filesData.length === 0) {
+                    clickTime += 1;
+                    if (clickTime > 4) {
+                        alert("检测到您多次点击上传没有反映, 如果您使用的是微信, 请点击右上角, 更换为浏览器浏览. 如果您已经在使用浏览器了, 那您当前的浏览器无法在上传文件. 请更换浏览器或设备");
+                    }
+                } else {
+                    clickTime  = false;
+                }
+            }
+        };
+
+        document.getElementById('deleteMode').onclick = function() {
+            var uploadButton = document.getElementById('uploadButton'),
+                finishEle = document.getElementsByClassName('finish'),
+                i, l;
+            if (deleteMode) {
+                uploadButton.innerHTML = '点击上传';
+                uploadButton.classList.remove('delete1');
+                uploadButton.classList.remove('delete2');
+                upload.removeAttribute('hidden');
+                for (i = 0, l = finishEle.length; i < l; i += 1) {
+                    finishEle[i].removeAttribute('hidden');
+                }
+                deleteMode = false;
+                deleteTrack = [];
+            } else {
+                uploadButton.innerHTML = '删除选中项';
+                uploadButton.classList.add('delete1');
+                upload.setAttribute('hidden', 'hidden');
+                for (i = 0, l = finishEle.length; i < l; i += 1) {
+                    finishEle[i].setAttribute('hidden', 'hidden');
+                }
+                deleteMode = true;
+            }
+            refreshEle();
+        };
+
+        board.addEventListener('click', function(e){
+            e = e || window.event;
+            var target = e.target || e.srcElement,
+                tagClass = target.parentNode.className;
+            var ele = tagClass.match('imageDiv') ? target.parentNode : target.className.match('imageDiv') ? target : null;
+            if (ele) {
+                if (deleteMode) {
+                    var uploadButton = document.getElementById('uploadButton');
+                    var value = ele.getAttribute('value');
+                    for (var i = 0, l = deleteTrack.length; i < l; i += 1) {
+                        if (deleteTrack[i] === value) {
+                            deleteTrack.splice(i, 1);
+                            ele.classList.remove('delete');
+                            if(deleteTrack.length === 0) {
+                                uploadButton.classList.remove('delete2');
+                                uploadButton.classList.add('delete1');
+                            }
+                            return;
+                        }
+                    }
+                    ele.classList.add('delete');
+                    deleteTrack.push(value);
+                    if (deleteTrack.length === 1) {
+                        uploadButton.classList.remove('delete1');
+                        uploadButton.classList.add('delete2');
+                    }
+                }
+            }
+        }, false);
+
+        function appendImgEle (file) {
+            var imageDiv = document.createElement('div');
+            imageDiv.style.width = size;
+            imageDiv.style.height = size;
+            imageDiv.className = 'board_upload_div imageDiv';
+            imageDiv.setAttribute('value', file.no);
+            var imageEle = document.createElement('img');
+            imageEle.setAttribute('src', file.url);
+            imageDiv.appendChild(imageEle);
+            if (board.lastChild == upload) {
+                board.insertBefore(imageDiv);
+            } else {
+                board.insertBefore(imageDiv, upload.nextElementSibling);
+            }
+
+            refreshEle ();
+            imgLoad(imageEle, function(img){
+                var proportion = img.width / img.height, a;
+                if (proportion > 1) {
+                    a = (sizeNum * proportion - sizeNum) / 2;
+                    img.style.marginLeft = -a + 'px';
+                    img.className = 'height';
+                } else {
+                    a = (sizeNum / proportion - sizeNum) / 2;
+                    img.style.marginTop = -a + 'px';
+                    img.className = 'width';
+                }
+            });
+            file.ele = imageDiv;
+        }
+
+        function cleanImgEle (img) {
+                if (img) {
+                    img.parentNode.removeChild(img);
+                } else {
+                    var images = document.getElementsByClassName('imageDiv');
+                    for (var i = 0, l =images.length; i < l; i += 1) {
+                        images[0].parentNode.removeChild(images[0]);
+                    }
+                }
+        }
+
+        function refreshEle (sign) {
+            if (!sign) {
+                var images = deleteMode ? document.getElementsByClassName('imageDiv') : board.children;
+                for (var i = 0, l = images.length; i < l; i += 1) {
+                    if ((i + 1) % 3 === 0) {
+                        images[i].style.margin = 0;
+                    } else {
+                        images[i].style.margin = "";
+                    }
+                    images[i].classList.remove('delete');
+                }
+            } else {
+                images = board.children;
+                var imagesMirror = [];
+                for (i = 0, l = images.length; i < l;i += 1) {
+                    if (images[i].className.match('finish')) {
+                        imagesMirror.push(images[i]);
+                    } else {
+                        imagesMirror.splice(1, 0, images[i]);
+                    }
+                }
+                board.innerHTML = '';
+                for (i = 0; i < l;i += 1) {
+                    board.appendChild(imagesMirror[i]);
+                    if ((i + 1) % 3 === 0) {
+                        imagesMirror[i].style.margin = 0;
+                    } else {
+                        imagesMirror[i].style.margin = "";
+                    }
+                }
+            }
+
+        }
+
+        function imgLoad (img, callback) {
+            toolByPandaLv.loadingPage.loadingPageOpen();
+            var timer = setInterval(function() {
+                if (img.complete){
+                    toolByPandaLv.loadingPage.loadingPageClose();
+                    callback(img);
+                    clearInterval(timer);
+                }
+            }, 50)
+        }
+
+        function createObjectURL(blob){
+            if (window.URL){
+                return window.URL.createObjectURL(blob);
+            } else if (window.webkitURL){
+                return window.webkitURL.createObjectURL(blob);
+            } else {
+                return null;
+            }
+        }
+
+        document.getElementById('uploadButton').onclick = function() {
+            if (deleteMode) {
+                if (!deleteTrack.length) {
+                    return false;
+                } else {
+                    if (window.confirm('确认删除?')) {
+                        deleteTrack.forEach(function(no){
+                            for (var i = 0, l = filesData.length; i < l; i += 1) {
+                                if (filesData[i].no == no) {
+                                    cleanImgEle(filesData[i].ele);
+                                    filesData.splice(i, 1);
+                                    break;
+                                }
+                            }
+                        });
+                        document.getElementById('deleteMode').click();
+                    }
+                }
+            } else {
+                if (filesData.length) {
+                    var i = 0,
+                        len = filesData.length,
+                        eleArray = [],
+                        failArray = [];
+                    while (i < len) {
+                        eleArray.push(filesData[i].ele);
+                        delete (filesData[i].ele);
+                        i += 1;
+                    }
+                    toolByPandaLv.loadingPage.loadingPageOpen({text:'上传中', mode:'progress'});
+                    uploadFile_ajax(filesData, 0, len, eleArray, failArray);
+                } else {
+                    alert('没有图片可以上传');
+                }
+            }
+        };
+        function uploadFile_ajax(filesData, i, len, eleArray, failArray) {
+            var data = new FormData();
+            data.append("file" + i, filesData[i]);
+            toolByPandaLv.myAjax({
+                method: 'post',
+                url: '',
+                data: data,
+                success: function(response) {
+                    i += 1;
+                    if (response.success) {
+                        toolByPandaLv.loadingPage.processChange(i, len);
+                        eleArray[i - 1].classList.add('finish');
+                        filesData.splice(i - 1, 1);
+                        eleArray.splice(i - 1, 1);
+                    } else {
+                        filesData[i - 1].ele = eleArray[i - 1];
+                        failArray.push({
+                            no : i - 1,
+                            errorCode: response.errorCode,
+                            message: response.message
+                        });
+                    }
+                    if (i === len) {//上传结束
+                        //刷新input的file列表
+                        upload.innerHTML = upload.innerHTML;
+                        if (failArray) {
+                            refreshEle(true);
+                            alert('部分图像上传失败');
+                        } else {
+                            alert('上传成功');
+                        }
+                        toolByPandaLv.loadingPage.loadingPageClose();
+                    } else {
+                        uploadFile_ajax(filesData[i], i, len, eleArray. failArray);
+                    }
+                },
+                error: function(){
+                    var i = 0, len = filesData.length;
+                    while (i < len) {
+                        filesData[i].ele = eleArray[i];
+                        i += 1;
+                    }
+                    toolByPandaLv.loadingPage.loadingPageClose();
+                    alert('上传错误');
+                },
+                timeout: function() {
+                    var i = 0, len = filesData.length;
+                    while (i < len) {
+                        filesData[i].ele = eleArray[i];
+                        i += 1;
+                    }
+                    toolByPandaLv.loadingPage.loadingPageClose();
+                    alert('超时');
+                }
+
+            });
+        }
+    }
+})();
